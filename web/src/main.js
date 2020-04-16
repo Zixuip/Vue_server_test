709da8afe5580621f52d8aa862e7fc43ae22cd39
Zixuip <854066271@qq.com>
2020-04-16 10:59:47 +0800

    修改

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
+// 引入全部组件
+import Mint from 'mint-ui';
+Vue.use(Mint);
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
